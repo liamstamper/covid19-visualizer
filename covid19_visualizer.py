05aacf9446fff750a0bee436d5290ed65c3e2e41
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2023-11-17 15:53:40 -0500

    country dropdown selector added

--- covid19_visualizer.py
+++ covid19_visualizer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import plotly.express as px
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, request
 
 # Load COVID-19 data
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
@@ -10,11 +10,13 @@ df = pd.read_csv(url)
 app = Flask(__name__)
 
 # Define a route to render the HTML page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def render_dashboard():
-    # Filter data for a specific country
-    country_name = 'United States'
-    country_data = df[df['Country/Region'] == country_name]
+    # Get the selected country from the dropdown (default to United States)
+    selected_country = request.form.get('country', 'United States')
+
+    # Filter data for the selected country
+    country_data = df[df['Country/Region'] == selected_country]
 
     # Extract the date columns
     dates = pd.to_datetime(country_data.columns[4:], format='%m/%d/%y')
@@ -23,14 +25,26 @@ def render_dashboard():
     cases = country_data.iloc[:, 4:].sum().astype(int).values
 
     # Create an interactive Plotly chart
-    fig = px.line(x=dates, y=cases, markers=True, title=f"COVID-19 Cases in {country_name}")
+    fig = px.line(x=dates, y=cases, markers=True, title=f"COVID-19 Cases in {selected_country}")
     fig.update_xaxes(title_text="Date")
     fig.update_yaxes(title_text="Total Cases")
+
+    # Customize chart appearance
+    fig.update_traces(line=dict(width=2))
+    fig.update_layout(
+        xaxis=dict(showgrid=True, gridwidth=0.5, gridcolor='lightgray'),
+        yaxis=dict(showgrid=True, gridwidth=0.5, gridcolor='lightgray'),
+        paper_bgcolor='white'
+    )
 
     # Convert the Plotly figure to HTML
     plot_html = fig.to_html(full_html=False)
 
-    # Define an HTML template
+    # Define a list of country options for the dropdown
+    country_options = df['Country/Region'].unique()
+    country_options.sort()
+
+    # Define an HTML template with the dropdown
     template = f'''
     <!DOCTYPE html>
     <html>
@@ -40,6 +54,13 @@ def render_dashboard():
     </head>
     <body>
         <h1>COVID-19 Data Visualization</h1>
+        <form method="post">
+            <label for="country">Select a Country:</label>
+            <select name="country" id="country">
+                {"".join([f'<option value="{country}" {"selected" if country == selected_country else ""}>{country}</option>' for country in country_options])}
+            </select>
+            <input type="submit" value="Submit">
+        </form>
         <div id="chartDiv">
             {plot_html}
         </div>
