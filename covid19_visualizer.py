5e7bce78df8658a3be3df2e75fbc559805b4cf10
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2023-11-30 13:02:38 -0600

    dark mode added and comparison of 2+ countries made possible

--- covid19_visualizer.py
+++ covid19_visualizer.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import plotly.graph_objects as go
 from flask import Flask, render_template_string, request
+import plotly.graph_objs as go
+from flask import Flask
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
 # Load COVID-19 data
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
@@ -12,8 +17,8 @@ app = Flask(__name__)
 # Define a route to render the HTML page
 @app.route('/', methods=['GET', 'POST'])
 def render_dashboard():
-    # Get the selected countries from the dropdowns (default to Afghanistan and Brazil)
-    selected_country1 = request.form.get('country1', 'Afghanistan')
+    # Get the selected countries from the dropdowns (default to Australia and Brazil)
+    selected_country1 = request.form.get('country1', 'Austrailia')
     selected_country2 = request.form.get('country2', 'Brazil')
 
     # Filter data for the selected countries
@@ -84,6 +89,7 @@ def render_dashboard():
                 margin-top: 30px;
                 border:1px solid lightgrey;
             }}
+            
         </style>
         <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
     </head>
@@ -109,5 +115,50 @@ def render_dashboard():
 
     return render_template_string(template)
 
+server = Flask(__name__)
+
+# Initialize Dash on your Flask server
+app = dash.Dash(__name__, server=server, routes_pathname_prefix='/')
+
+# Define a dark theme for your layout
+app.layout = html.Div(style={'backgroundColor': '#111', 'color': '#fff'}, children=[
+    html.H1('COVID-19 Cases Dashboard', style={'textAlign': 'center', 'color': '#7FDBFF'}),
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': country, 'value': country} for country in sorted(df['Country/Region'].unique())],
+        value=['Australia', 'Brazil'],  # default value
+        multi=True,
+        style={'backgroundColor': '#333', 'color': '#000000'}
+    ),
+    dcc.Graph(id='covid-cases-linechart'),
+])
+
+# Define callback to update graph
+@app.callback(
+    Output('covid-cases-linechart', 'figure'),
+    [Input('country-dropdown', 'value')]
+)
+def update_graph(selected_countries):
+    traces = []
+    for country in selected_countries:
+        country_data = df[df['Country/Region'] == country]
+        dates = pd.to_datetime(country_data.columns[4:], format='%m/%d/%y')
+        cases = country_data.iloc[:, 4:].sum().astype(int).values
+        traces.append(go.Scatter(x=dates, y=cases, mode='lines+markers', name=country))
+
+    figure = {
+        'data': traces,
+        'layout': go.Layout(
+            title='COVID-19 Cases Over Time',
+            xaxis={'title': 'Date'},
+            yaxis={'title': 'Number of Cases'},
+            paper_bgcolor='#111',
+            plot_bgcolor='#111',
+            font={'color': '#fff'},
+        ),
+    }
+    return figure
+
+# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    server.run(debug=True)
